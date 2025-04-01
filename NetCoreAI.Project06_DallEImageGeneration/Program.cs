e407b0e91ec0145654202f06f6df25cb97b86fe3
yusuf-bilgin <yusufbilgin1905@gmail.com>
2025-04-01 23:28:02 +0300

    dall e project added

---
+++ NetCoreAI.Project06_DallEImageGeneration/Program.cs
@@ -0,0 +1,24 @@
+﻿using System.Text;
+using Newtonsoft.Json;
+
+class Program
+{
+    public static async Task Main(string[] args)
+    {
+        string apiKey = "Your api key";
+        Console.WriteLine("Example promapts: ");
+        string propmt = Console.ReadLine();
+        using (HttpClient client = new HttpClient())
+        {
+            client.DefaultRequestHeaders.Add("Authorization", $"Bearer {apiKey}");
+            var requestBody = new
+            {
+                prompt = propmt,
+                n = 1,
+                size = "1024x1024"
+            };
+            string jsonBody = JsonConvert.SerializeObject(requestBody);
+            var content = new StringContent(jsonBody,Encoding.UTF8,"application/json");
+    }
+    }
+}
