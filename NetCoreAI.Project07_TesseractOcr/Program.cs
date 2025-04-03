905f508e95b96b1d8cbc7d5db62065afff5e97c8
yusuf-bilgin <yusufbilgin1905@gmail.com>
2025-04-03 16:25:58 +0300

    tesseract app is done

--- NetCoreAI.Project07_TesseractOcr/Program.cs
+++ NetCoreAI.Project07_TesseractOcr/Program.cs
@@ -1,2 +1,51 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Tesseract;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        string tessDataPath = @"D:\TesseractOcrTraineddata";
+
+        while (true)
+        {
+            Console.WriteLine("Karakter okuması yapılacak görsel yolu: ");
+            string imagePath = Console.ReadLine();
+            Console.WriteLine();
+
+            if (string.IsNullOrWhiteSpace(imagePath))
+            {
+                Console.WriteLine("Geçersiz giriş! Lütfen geçerli bir görsel yolu girin.");
+                continue;
+            }
+
+            try
+            {
+                using (var engine = new TesseractEngine(tessDataPath, "eng", EngineMode.Default))
+                {
+                    using (var img = Pix.LoadFromFile(imagePath))
+                    {
+                        using (var page = engine.Process(img))
+                        {
+                            string text = page.GetText();
+                            Console.WriteLine("Görselden Okunan Metin: ");
+                            Console.WriteLine(text);
+                        }
+                    }
+                }
+            }
+            catch (Exception exception)
+            {
+                Console.WriteLine($"Bir hata oluştu: {exception.Message}");
+            }
+
+            Console.WriteLine("\nYeni bir görsel yolu girmek için bir tuşa basın. Çıkmak için ESC tuşuna basın.");
+            var key = Console.ReadKey(true);
+
+            if (key.Key == ConsoleKey.Escape)
+            {
+                Console.WriteLine("Çıkış yapılıyor...");
+                break;
+            }
+        }
+    }
+}
